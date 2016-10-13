38a39d7c18f2f8e38d60d1ec7a1e5aba98359ee6
ziggurat777 <ziggurat73128@gmail.com>
2016-10-13 17:34:29 +0800

    set environment

---
+++ SuperMan/App_Start/BundleConfig.cs
@@ -0,0 +1,20 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace SuperMan
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery", "https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js").Include("~/Content/Script/jquery/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap", "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js").Include("~/Content/Script/bootstrap/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/Css", "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css").Include("~/Content/Css/bootstrap/bootstrap.css"));
+
+            BundleTable.EnableOptimizations = true;
+            bundles.UseCdn = true;
+        }
+    }
+}
